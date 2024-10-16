c3d16a1ddc504de4ceb3cf436fb64c8ded0735e0
Felix Gateru <felix.gateru@gmail.com>
2024-10-16 12:08:31 +0300

    refactor(middleware): group service middleware
    Signed-off-by: Felix Gateru <felix.gateru@gmail.com>

--- cmd/coap/main.go
+++ cmd/coap/main.go
@@ -145,7 +145,7 @@ func main() {
 
 	svc := coap.New(clientsClient, channelsClient, nps)
 
-	svc = tracing.New(tracer, svc)
+	svc = middleware.TracingMiddleware(tracer, svc)
 
 	svc = httpapi.LoggingMiddleware(svc, logger)
 
